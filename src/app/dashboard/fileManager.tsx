0cb5437e6d221ee572097399da8390ff87c5fe88
Ayush-478 <147281294+Ayush-478@users.noreply.github.com>
2025-11-27 22:53:30 +0530

    Update fileManager.tsx

--- src/app/dashboard/fileManager.tsx
+++ src/app/dashboard/fileManager.tsx
@@ -72,7 +72,7 @@ export default function FileManager({reload, setReload, relativePath, setRelativ
   const [allFiles, setAllFiles] = useState<FileItem[]>([])
   const [renderedFiles, setRenderedFiles] = useState<string[]>([])
   const [, offlineReload] = useState(0)
-  const [selectedArray, setSelectedArray] = useState<string[]>([])
+  const [selectedArray, setSelectedArray] = useState<JSX.Element[]>([])
 
   useEffect(()=>{
     (async()=>{
