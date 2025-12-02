59838bd15c13b12aced2e32fda20b482642d4a44
Ayush-478 <ayushchauhan2302@gmail.com>
2025-12-02 12:30:08 +0530

    fixed metadata

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,12 +4,15 @@ import { useState } from 'react'
 import NavMenu from './navmenu'
 import FileTree from './filetreeSidebar'
 import Explorer from './explorer'
+import Head from 'next/head';
+
 
 export default function Dashoard(){
   const [query, setQuery] = useState("")
   const [reloadFileTree, setReloadFileTree] = useState(false)
   return(
   <div className="flex flex-col dark bg-background text-foreground h-lvh w-lvw">
+      <Head><title>Dashoard - Raft</title></Head>
       <NavMenu setQuery = {setQuery} query = {query}></NavMenu>
       <div className="main flex h-[91%] w-lvw">
         <FileTree></FileTree>
